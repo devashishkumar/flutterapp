e412b4fa49bfc16e7ac56f43da6ba059e790106e
Ashish K <dev.ashishkumar@gmail.com>
2021-07-01 12:06:56 +0530

    add homepage class along with drawer and app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:practiceapp/homepage.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  // const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     // return Container();
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Material(
-        child: Center(
-            child: Container(
-                child: Text('Welcome to 30 days of flutter')
-            )
-        )
-      ),
+      home: HomePage()
     );
   }
 }
